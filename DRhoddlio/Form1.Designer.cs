595ed6d2e2ab07305965a18e44355c9063cb5552
Declan Rhodes <declanrhodesss@gmail.com>
2019-10-31 13:00:02 +0000

    Comments

--- DRhoddlio/Form1.Designer.cs
+++ DRhoddlio/Form1.Designer.cs
@@ -171,7 +171,7 @@
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "DRhoddlio";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
--- DRhoddlio/Form1.cs
+++ DRhoddlio/Form1.cs
@@ -15,20 +15,27 @@ namespace DRhoddlio
 {
     public partial class Form1 : Form
     {
+        //assigning variables 
         int x = 0;
         int y = 0;
         public int[,] gameBoardArr;
         GImageArray gameBoard;
+        //Setting the directory that the images can be found in for the array and the GUI
         string imagesDir = Directory.GetCurrentDirectory() + "\\images\\";
 
         public Form1()
         {
             InitializeComponent();
+            //Set the images in the GUI
+            pictureBox2.Image = Image.FromFile(imagesDir + "0.png");
+            pictureBox3.Image = Image.FromFile(imagesDir + "1.png");
         }
 
         public void populateGameBoardList()
         {
+            //Crate a new array for the game board
             gameBoardArr = new int[8, 8];
+            //Loop round all the rows and columnds setting the values to 10 for the default picture
             while (x < 8)
             {
                 y = 0;
@@ -40,25 +47,25 @@ namespace DRhoddlio
                 }
                 x++;
             }
+            //Sets the values for the four middle counters in the field.
             gameBoardArr[3, 3] = 1;
             gameBoardArr[3, 4] = 0;
             gameBoardArr[4, 4] = 1;
             gameBoardArr[4, 3] = 0;
 
+            //Loads the gameBoard array and creates a GImageArray object under the variable of gameBoard
             gameBoard = new GImageArray(this, gameBoardArr, 150, 0, 7, 7, 5, imagesDir);
+            //Create an event handler for when a cell in the game board is clicked. 
             gameBoard.Which_Element_Clicked += new GImageArray.ImageClickedEventHandler(Which_Element_Clicked);
-        }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-            populateGameBoardList();
         }
 
         private void Which_Element_Clicked(object sender, EventArgs e)
         {
+            //Returns the row and column of the clicked cell
             int col = gameBoard.Get_Col(sender);
             int row = gameBoard.Get_Row(sender);
 
+            //Determines current player based on the label for the current player, sets the appropriate value and then changes to the other player's turn
             if (currentPlayer1.Visible == true)
             {
                 gameBoardArr[row, col] = 1;
@@ -71,15 +78,17 @@ namespace DRhoddlio
                 currentPlayer0.Visible = false;
                 currentPlayer1.Visible = true;
             }
-
+            //Updates the images in the board with the new values. 
             gameBoard.UpDateImages(gameBoardArr);
         }
 
         private void newGameToolStripMenuItem_Click(object sender, EventArgs e)
         {
+            //Create the strings for the player names
             string playerBName = "";
             string playerWName = "";
 
+            //Make sure the users have input a player name for both players
             while (string.IsNullOrEmpty(playerWName) || string.IsNullOrWhiteSpace(playerWName))
             {
                 playerWName = My_Dialogs.InputBox("Enter the White player's name");
@@ -89,10 +98,16 @@ namespace DRhoddlio
                 playerBName = My_Dialogs.InputBox("Enter the Black player's name");
             }
             
+            //runs the command to populate the game board and then sets the player names to be the appropriate players before indicating that white starts the game. 
             populateGameBoardList();
             bPlayerTxt.Text = playerBName;
             wPlayerTxt.Text = playerWName;
             currentPlayer1.Visible = true;
+        }
+
+        private void isValidMove()
+        {
+            int[,] dirToCheckArr = new int[8, 2] { { 0, 1 }, { 1, 1 }, { 1, 0 }, { 1, -1 }, { 0, -1 }, { -1, -1 }, { -1, 0 }, { -1, 1 } };
         }
     }
 }
